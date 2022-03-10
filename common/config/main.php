9a8de28b90b03255c9a013a43e2a1ab14764e841
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-03-10 10:49:03 +0700

    Install yii2-ajaxcrud
    - Support bs4
    - Support Indonesia language

--- common/config/main.php
+++ common/config/main.php
@@ -18,7 +18,17 @@ return [
         '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'language' => 'id-ID',
     'components' => [
+        'i18n' => [
+            'translations' => [
+                'yii2-ajaxcrud' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => '@yii2ajaxcrud/ajaxcrud/messages',
+                    'sourceLanguage' => 'id',
+                ],
+            ]
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
