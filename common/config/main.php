2b42c6cf74393927329488138131e342f251ab63
Syahrizal Hanif <syahrizalhanif@gmail.com>
2022-03-10 10:29:31 +0700

    Install kartik yii2-grid

--- common/config/main.php
+++ common/config/main.php
@@ -1,8 +1,21 @@
 <?php
+
+use kartik\grid\Module;
+
 return [
+    'name' => 'SSO - ITS PKU Muhammadiyah Surakarta',
+    'timeZone' => 'Asia/Jakarta',
+    'modules' => [
+        'gridview' => [
+            'class' => Module::class
+        ],
+        'gridviewKrajee' => [
+            'class' => Module::class
+        ],
+    ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
